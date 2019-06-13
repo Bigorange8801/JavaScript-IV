bf76f42149805be9fefaf6b5f2df9dd6450b4968
John <cjd8801@gmail.com>
2019-06-13 16:02:33 -0500

    class build

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,3 +1,21 @@
 // CODE here for your Lambda Classes
 
-// Starting commit
+const fred = new Instructor({
+    name: 'Fred',
+    location:'Bedrock',
+    age:37,
+    favLanguage: 'JavaScript',
+    specialty: 'Front-end',
+    catchPhrase: "Don't for get the homies"
+});
+
+class Person {
+    constructor(attributes) {
+        this.name=attributes.name;
+        this.age=attributes.age;
+        this.location=at.location;
+    }
+    speak() {
+        return `Hello, my name is ${this.name}, I am from${this.location}`;
+    }
+}
